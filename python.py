f1c53a818b50e3d6361fb059d732ea7f95f1dc42
sxd7 <76768368+sxd7@users.noreply.github.com>
2020-12-30 13:00:42 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,9 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> radius=float(input("enter the radius of the circle:"))
+enter the radius of the circle:5
+>>> area=math.pi*radius*radius;
+>>> print("area of the circle is:{0}".format(area));
+area of the circle is:78.53981633974483
+>>> 
